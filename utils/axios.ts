db67376a89dca492de87eae840bd93b8cc4fe276
Sauban <sauban.git@gmail.com>
2025-11-24 12:56:31 +0530

    fix: axios to https

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,15 +1,6 @@
-
 import axios from "axios";
-import https from "https";
-
-const agent = new https.Agent({
-  rejectUnauthorized: false, // ignore self-signed certificate
-});
-
 const instance = axios.create({
-  baseURL: "https://192.168.31.54:3000/api/v1",
-  httpsAgent: agent,
-});
+  baseURL: `https://192.168.31.54:3000/api/v1`,
+})
 
-export default instance;
-
+export default instance
