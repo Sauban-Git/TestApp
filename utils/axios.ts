dd0cf73720f7ac4f7c664ce0bc7026bab36c30d9
Sauban <sauban.git@gmail.com>
2025-11-24 13:04:38 +0530

    fix: axios to https

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: `https://192.168.31.54:3000/api/v1`,
+  baseURL: `http://192.168.31.54:3000/api/v1`,
 })
 
 export default instance
